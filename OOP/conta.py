74ab916e1c20f2daf40fd1c7f24fb7a795abf228
BrunoCilento <75695111+BrunoCilento@users.noreply.github.com>
2022-06-20 09:07:32 -0300

    1

--- OOP/conta.py
+++ OOP/conta.py
@@ -4,10 +4,22 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo um objeto {}".format(self))
-        self.numero = numero #este objeto vai ter um atributo chamado número
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero #este objeto vai ter um atributo chamado número
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
+    def extrato(self):
+        print("O titular {} tem {} de saldo".format(self.__titular, self.__saldo))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+    
+    def saca(self,valor):
+        self.__saldo -= valor
+    
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
 
 
